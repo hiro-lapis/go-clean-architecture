cc32dcea15a3ebf046e0a1bf74a6482623db8310
hiro-lapis <coolish555ddddd@gmail.com>
2024-01-02 17:08:10 +0900

    add constructor

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,9 +1,25 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"clean-architecture/usecase"
 
+	"github.com/labstack/echo/v4"
+)
+
+// IUserController is an interface for user controller
+// recieve request as echo.Context
 type IUserController interface {
 	SignUp(c echo.Context) error
 	Login(c echo.Context) error
 	LogOut(c echo.Context) error
+}
+
+// UserController is a struct for user controller
+// return is depend on usecase interface
+type UserController struct {
+	uu usecase.IUserUsecase
+}
+
+func NewUserConteroller(uu usecase.IUserUsecase) IUserController {
+	return &UserController(uu)
 }
