7db5ee34b515e1df49dd59c8beb28826f583868b
hiro-lapis <coolish555ddddd@gmail.com>
2024-01-02 10:47:18 +0900

    add implement struct user repository

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,10 +1,23 @@
 package repository
 
-import "clean-architecture/model"
+import (
+	"clean-architecture/model"
+
+	"gorm.io/gorm"
+)
 
 // DBとのやりとりを介したCRUD処理のinterface定義をしているパッケージ
 // usecaseから呼び出され、実装している実structとの仲介を行う
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+
+// userRepository is a struct that implements the IUserRepository interface.
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository(db)
 }
