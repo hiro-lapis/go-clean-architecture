0ad4767e14ca7792218ece3924cd5486d542e6d2
hiro-lapis <coolish555ddddd@gmail.com>
2024-01-03 10:54:12 +0900

    preimplement task controller

---
+++ controller/task_controller.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"clean-architecture/usecase"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ITaskController interface {
+	GetAllTasks(c echo.Context) error
+	GetTaskById(c echo.Context) error
+	CreateTask(c echo.Context) error
+	UpdateTask(c echo.Context) error
+}
+
+type taskController struct {
+	tu usecase.ITaskUsecase
+}
+
+func NewTaskController(tu usecase.ITaskUsecase) ITaskController {
+	return &taskController{tu}
+}
+
+// func (tc *taskController) error {}
