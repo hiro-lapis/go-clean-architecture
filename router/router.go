c9eb3470ac437b56570425933961505e7c8adec3
hiro-lapis <coolish555ddddd@gmail.com>
2024-01-16 21:52:18 +0900

    modify middleware, add cors, csrf

--- router/router.go
+++ router/router.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"clean-architecture/controller"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
@@ -15,6 +17,36 @@ func NewRouter(
 	tc controller.ITaskController,
 ) *echo.Echo {
 	e := echo.New()
+	// CORS設定(origin,ヘッダ)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		// アクセスを許可するドメイン
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		// 設定許可するHeader属性
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders,
+			//　ヘッダ経由でCSRFトークンを取得
+			echo.HeaderXCSRFToken,
+		},
+		// 許可するHTTP method
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+	}))
+	// CSRF設定
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		// トークンはドメインのroot pathに設定
+		CookiePath:   "/",
+		CookieDomain: os.Getenv("API_DOMAIN"),
+		// jsスクリプトからの読取拒否
+		CookieHTTPOnly: true,
+		// prod:SameSiteNoneMode(強制的にsecure=true) dev:SameSiteDefaultMode(postmanデバッグ)
+		//CookieSameSite: http.SameSiteNoneMode,
+		CookieSameSite: http.SameSiteDefaultMode,
+		//CookieMaxAge: 60,
+	}))
+	e.GET("/csrf", uc.CsrfToken)
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 	e.POST("/logout", uc.LogOut)
