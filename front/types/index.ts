db55a03b76b8c4b4b75e82075988c04cf575c7c7
hiro-lapis <coolish555ddddd@gmail.com>
2024-01-20 02:10:52 +0900

    add types

---
+++ front/types/index.ts
@@ -0,0 +1,16 @@
+export type Task {
+    id: number
+    title: string
+    descripition: string
+    created_at: Date
+    updated_at: Date
+}
+
+export type CsrfToken {
+    csrf_token: string
+}
+
+export type Credential {
+    email: string
+    password: string
+}
