c82c2fb4fb84129984f12a3e89c9e5c09c3338e4
hiro-lapis <coolish555ddddd@gmail.com>
2024-02-03 08:15:47 +0900

    implement logout func

--- front/src/components/Todo.tsx
+++ front/src/components/Todo.tsx
@@ -1,5 +1,21 @@
+import { ArrowRightCircleIcon, ShieldCheckIcon } from "@heroicons/react/24/solid"
+import {
+  useMutateAuth,
+} from '../hooks/useMutateAuth'
+
 export const Todo = () => {
+  const { logoutMutation } = useMutateAuth()
+  const logout = async () => {
+    // Similar to mutate but returns a promise which can be awaited.
+    // https://tanstack.com/query/v5/docs/framework/react/reference/useMutation
+    await logoutMutation.mutateAsync()
+  }
   return (
-    <div>Todo</div>
+    <div>
+      <ArrowRightCircleIcon
+        onClick={ logout }
+        className="h-8 w-8 mr-2 text-blue-500 cursor-pointer"
+      />
+    </div>
   )
 }
